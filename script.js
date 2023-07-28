a717a8ed9a7cd4a99f082326c13cfee68605bcdb
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-07-28 17:17:48 +0200

    Edited password validation

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-// Get input elements and error message containers
+//compare two passwords for being identical
 const passwordInput = document.getElementById('pass');
 const confirmPasswordInput = document.getElementById('passRep');
 const passwordError = passwordInput.nextElementSibling.firstChild;
 const confirmError = confirmPasswordInput.nextElementSibling.firstChild;
 
-// Add an input event listener to both password fields
 passwordInput.addEventListener('input', validatePasswords);
 confirmPasswordInput.addEventListener('input', validatePasswords);
 
 function validatePasswords() {
-    // Check if the password field is empty
+
     if (passwordInput.value === '') {
         passwordError.style.color = 'red';
     } else {
         passwordError.style.color = 'transparent';
     }
     
-    // Check if passwords match
     if (passwordInput.value !== confirmPasswordInput.value) {
-        // If they don't match, show the error message
         confirmError.style.color = 'red';
     } else {
-        // If they match or are empty, hide the error message
         confirmError.style.color = 'transparent';
     }
 }
